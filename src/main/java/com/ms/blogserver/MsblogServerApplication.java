8a6ed6560f1783a1714154f545434e335a3ea15f
maoshen5478 <361193645@qq.com>
2021-06-06 16:30:09 +0800

    Modify packaging method war

--- src/main/java/com/ms/blogserver/MsblogServerApplication.java
+++ src/main/java/com/ms/blogserver/MsblogServerApplication.java
@@ -3,9 +3,16 @@ package com.ms.blogserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MsblogServerApplication {
+public class MsblogServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MsblogServerApplication.class);
+    }
 
     public static void main(String[] args) {
         //SpringApplication.run(MsblogServerApplication.class, args);
