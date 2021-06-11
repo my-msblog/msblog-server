d41b4f74275c779feb53fb515fdba71776766bc5
Admin <361193645@qq.com>
2021-06-11 15:20:45 +0800

    add tag

--- src/main/java/com/ms/blogserver/entity/Tags.java
+++ src/main/java/com/ms/blogserver/entity/Tags.java
@@ -1,9 +1,19 @@
 package com.ms.blogserver.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * @description:
  * @author: zhh
  * @time: 2021/6/11
  */
+@Data
+@ToString
+@TableName(value = "")
 public class Tags {
+    private Long id;
+    private String name;
+    private String nameZh;
 }
