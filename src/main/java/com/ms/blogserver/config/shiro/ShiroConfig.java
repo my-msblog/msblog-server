2e846fb983201b5a209b0809b4519e212f89fbb0
maoshen5478 <361193645@qq.com>
2021-07-23 11:15:57 +0800

    Release interfaces that do not require permissions

--- src/main/java/com/ms/blogserver/config/shiro/ShiroConfig.java
+++ src/main/java/com/ms/blogserver/config/shiro/ShiroConfig.java
@@ -46,6 +46,8 @@ public class ShiroConfig {
         filterRuleMap.put("/login", "anon");
         filterRuleMap.put("/logout", "anon");
         filterRuleMap.put("/code/**","anon");
+        filterRuleMap.put("/article/**","anon");
+        filterRuleMap.put("/user","anon");
         // 所有请求通过JWT Filter
         filterRuleMap.put("/**", "jwt");
         factoryBean.setFilterChainDefinitionMap(filterRuleMap);
