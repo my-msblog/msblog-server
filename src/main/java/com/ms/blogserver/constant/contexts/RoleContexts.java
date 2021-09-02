18d3b5535cb3069c43e9d60cedefded59aaa115a
maoshen5478 <361193645@qq.com>
2021-09-02 15:53:05 +0800

    role fields

--- src/main/java/com/ms/blogserver/constant/contexts/RoleContexts.java
+++ src/main/java/com/ms/blogserver/constant/contexts/RoleContexts.java
@@ -10,17 +10,17 @@ public interface RoleContexts {
     /**
      * 系统管理员
      */
-    String SYSTEM_ADMIN = "sysAdmin";
+    String SYSTEM_ADMIN = "SYSTEM_ADMIN";
     Long SYSTEM_ADMIN_ID = 1L;
     /**
      * 内容管理员
      */
-    String CONTENT_MANAGER = "contentManager";
+    String CONTENT_MANAGER = "CONTENT_MANAGER";
     Long CONTENT_MANAGER_ID = 2L;
     /**
      * 访客
      */
-    String VISITOR = "visitor";
+    String VISITOR = "VISITOR";
     Long VISITOR_ID = 3L;
 }
 
